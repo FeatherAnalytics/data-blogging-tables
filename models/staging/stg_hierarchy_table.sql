6349e7031ac129eb591307175beada77379153b4
HardageProjects <featheranalytics@proton.me>
2023-12-23 20:59:36 -0600

    staging hierarchy_table

--- models/staging/stg_hierarchy_table.sql
+++ models/staging/stg_hierarchy_table.sql
@@ -1,1 +1,11 @@
-select * from {{ source("gsheets","hierarchy_table") }}
+SELECT
+        _ROW
+    ,   ID
+    ,   NAME
+    ,   TITLE
+    ,   LAYER
+    ,   MANAGER_ID
+    ,   PARSE_DATE('%m/%d/%Y', START_DATE) AS START_DATE
+    ,   PARSE_DATE('%m/%d/%Y', END_DATE) AS END_DATE 
+FROM
+        {{ source("gsheets","hierarchy_table") }}
